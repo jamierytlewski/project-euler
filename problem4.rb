0f3f1a6c1ebfa4d2eacccb1e9d4eeff6aab9135f
Jamie R Rytlewski <jamier.net@gmail.com>
2011-05-25 01:46:05 -0400

    Made a slight change to make it faster

--- problem4.rb
+++ problem4.rb
@@ -3,6 +3,7 @@ palindrome = 0
 999.downto(100).each do |a|
 	999.downto(100).each do |b|
 		sum = a * b
+		break if sum < palindrome
 		if sum.to_s() == sum.to_s().reverse and sum > palindrome
 			palindrome = sum
 		end
