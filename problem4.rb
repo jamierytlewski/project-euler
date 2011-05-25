80a739050778f1ba37212e95d22b8db276ae793f
Jamie R Rytlewski <jamier.net@gmail.com>
2011-05-25 01:37:15 -0400

    My solving of problem 4

---
+++ problem4.rb
@@ -0,0 +1,11 @@
+a = b =  999
+palindrome = 0
+999.downto(100).each do |a|
+	999.downto(100).each do |b|
+		sum = a * b
+		if sum.to_s() == sum.to_s().reverse and sum > palindrome
+			palindrome = sum
+		end
+	end
+end
+puts palindrome.to_s().reverse
